628a5fbed73a5a17d6104ab090767da9bd51bd9f
ElianFelix <38637561+ElianFelix@users.noreply.github.com>
2023-03-23 13:08:54 -0400

    Add triangle_area

---
+++ triangle_area.py
@@ -0,0 +1,17 @@
+# Write a function that takes the base and height of a triangle 
+# and returns the area - triangle area = base * height / 2
+# ex:
+# triangle_area(3,2) -> 3
+# triangle_area(7,4) -> 14
+# triangle_area(10,10) -> 50
+
+def triangle_area(b, h):
+    return b * h / 2
+
+
+print(triangle_area(3, 2))
+
+print(triangle_area(7, 4))
+
+print(triangle_area(10, 10))
+
